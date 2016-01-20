8c0c7b39e9c5fbf98cb3e2fc305d9a0da5049864
K2791 <miro.ahola@gmail.com>
2016-01-20 10:19:00 +0200

    Changed Write.Line code

--- TestingApplication/Demo01_T6/Program.cs
+++ TestingApplication/Demo01_T6/Program.cs
@@ -15,12 +15,7 @@ namespace Demo01_T6
             double distance = double.Parse(line);
             double gas = (distance / 100 * 7.02);
             double money = (gas * 1.595);
-            Console.Write("Gas needed: ");
-            Console.Write(gas);
-            Console.Write("l");
-            Console.Write(", Money needed: ");
-            Console.Write(money);
-            Console.WriteLine("e");
+            Console.WriteLine("Gas needed: {0}l, money needed: {1}e", gas, money);
             Console.WriteLine("Press any key to continue...");
             Console.ReadLine();
         }
